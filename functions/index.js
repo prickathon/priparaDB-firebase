3e8b65eb7240a6458e6d1902b6646cd57cbd1255
takanakahiko <takanakahiko@gmail.com>
2018-04-12 23:36:03 +0900

    test

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
+exports.addCharacter = functions.https.onRequest((req, res) => {
+    const name = Object.keys(req.body)[0];
+    const info = req.body[name]
+    console.log(info);
+    return admin.database().ref('/characters/'+name).set(info).then((snapshot) => {
+        return res.redirect(303, snapshot.ref);
+    });
+});
