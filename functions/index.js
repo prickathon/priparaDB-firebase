fd0ac5070211ae9733438cc422142c8e2bb3eccf
takanakahiko <takanakahiko@gmail.com>
2018-04-25 12:18:58 +0900

    test

--- functions/index.js
+++ functions/index.js
@@ -15,11 +15,18 @@ exports.addCharacter = functions.https.onRequest((req, res) => {
     });
 });
 
-
-exports.setTeams = functions.database.ref('/characters/{characterId}/teams/{teamId}').onCreate((e) => {
+exports.OnSetTeamOfCharacter = functions.database.ref('/characters/{characterId}/teams/{teamId}').onCreate((e) => {
     return admin.database().ref(`/teams/${e.params.teamId}/members/${e.params.characterId}`).set(true);
 });
 
-exports.deleteTeams = functions.database.ref('/characters/{characterId}/teams/{teamId}').onDelete((e) => {
+exports.OnDeleteTeamOfCharacter = functions.database.ref('/characters/{characterId}/teams/{teamId}').onDelete((e) => {
     return admin.database().ref(`/teams/${e.params.teamId}/members/${e.params.characterId}`).set(null);
+});
+
+exports.OnSetCharacterOfTeam = functions.database.ref('/teams/{teamId}/members/{characterId}').onCreate((e) => {
+    return admin.database().ref(`/characters/${e.params.characterId}/teams/${e.params.teamId}`).set(true);
+});
+
+exports.OnDeleteCharacterOfTeam = functions.database.ref('/teams/{teamId}/members/{characterId}').onDelete((e) => {
+    return admin.database().ref(`/characters/${e.params.characterId}/teams/${e.params.teamId}`).set(null);
 });
