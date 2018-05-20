3de6cd739ebdf42c5f640005434d2f60fb631b6f
mtwtkman <punipuniomochi@gmail.com>
2018-05-20 17:18:56 +0900

    しゃーない

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,3 @@ RUN mkdir /app
 WORKDIR /app
 COPY . /app
 RUN npm i -g firebase-tools
-RUN cd /app/functions; npm i
--- d.ps1
+++ d.ps1
@@ -20,6 +20,7 @@ function npmrun($body)
 switch ($cmd) 
 {
     "build" {convert_win_path "docker build . -t $name"}
+    "init" {convert_win_path "docker run -ti --rm -v$($pwd.Path):/app $name sh -c 'cd functions; npm i'"}
     "run" {convert_win_path "docker run -ti -d --rm --name $name -v $($pwd.Path):/app -p 9005:9005 $name sh; docker exec -ti $name firebase login"}
     "stop" {convert_win_path "docker stop $name"}
     "serve" {convert_win_path(npmrun("serve:local"))}
