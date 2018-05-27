86c5043c43e7a8abe3f88b3151deac71a0aeacfe
takanakahiko <takanakahiko@gmail.com>
2018-05-28 02:18:09 +0900

    コンポーネントを分割

--- hosting/src/index.js
+++ hosting/src/index.js
@@ -1,18 +1,5 @@
-import firebase from 'firebase/app'
-import 'firebase/database'
-import 'firebase/auth'
-
-var config = {
-    apiKey: "AIzaSyDY30PPVzuDZYx92ONHKdqL0zXopHizDxw",
-    authDomain: "prickathon.firebaseapp.com",
-    databaseURL: "https://prickathon.firebaseio.com",
-    projectId: "prickathon",
-    storageBucket: "prickathon.appspot.com",
-    messagingSenderId: "854301723180"
-};
-var firebaseApp = firebase.initializeApp(config);
-
 import Vue from 'vue'
+import { firebaseApp } from './firebaseApp.js';
 
 Vue.config.devtools = true;
 
@@ -20,29 +7,6 @@ import VeeValidate from 'vee-validate';
 import VueFire from 'vuefire';
 import vSelect from 'vue-select'
 
-var provider = new firebase.auth.TwitterAuthProvider();
-
-firebase.auth().onAuthStateChanged(function(user) {
-	//authInfo(user);
-});
-
-//authInfo();
-
-function authInfo(user){
-	if (user) {
-		document.getElementById("auth_info").innerHTML = "ログイン中"
-	} else {
-		document.getElementById("auth_info").innerHTML = "ログインしていません"
-	}
-}
-
-function auth(){
-	firebase.auth().signInWithPopup(provider);
-}
-
-function signout(){
-	firebase.auth().signOut();
-}
 
 Vue.use(VeeValidate);
 Vue.use(VueFire);
